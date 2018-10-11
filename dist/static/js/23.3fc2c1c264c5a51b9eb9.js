cee4ef586b6e10115ce295def0da9ccca1c62f6b
pansijia <pansijia@sunlands.com>
2018-10-11 14:51:10 +0800

    fix 用户类型判断，微信分享配置，抽离配置功能

--- dist/static/js/23.3fc2c1c264c5a51b9eb9.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([23],{RUlc:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("section",[t("router-view")],1)},staticRenderFns:[]};var c=n("VU/8")({name:""},r,!1,function(e){n("gk5M")},"data-v-8c0707fc",null);t.default=c.exports},gk5M:function(e,t){}});
-//# sourceMappingURL=23.3fc2c1c264c5a51b9eb9.js.map
